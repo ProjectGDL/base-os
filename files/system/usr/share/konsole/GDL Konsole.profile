2e38a091f59d3d06928cd15c05bd787dcd9efa5f
Andrew <notrlt@proton.me>
2026-08-03 17:01:40 +0300

    Slight KDE customization

---
+++ files/system/usr/share/konsole/GDL Konsole.profile
@@ -0,0 +1,20 @@
+[Cursor Options]
+CursorShape=1
+
+[General]
+AlternatingBars=0
+Command=/bin/zsh
+ErrorBackground=0
+ErrorBars=0
+Name=GDL Konsole
+Parent=FALLBACK/
+SemanticHints=0
+TerminalMargin=10
+
+[Scrolling]
+HighlightScrolledLines=false
+HistoryMode=2
+
+[Terminal Features]
+AnimatingCursorEnabled=true
+BlinkingCursorEnabled=true
---
+++ files/system/usr/share/plasma/look-and-feel/GDL Dark/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,111 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "509",
+                            "popupWidth": "647"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "icon": "start-here-kde-plasma-symbolic",
+                            "systemFavorites": "suspend\\,hibernate\\,reboot\\,shutdown"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/General": {
+                            "launchers": "preferred://browser,applications:steam.desktop,applications:net.lutris.Lutris.desktop,applications:org.kde.konsole.desktop,applications:io.github.kolunmi.Bazaar.desktop,applications:io.github.ublue_os.yafti_gtk.desktop,preferred://filemanager"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        },
+                        "/Appearance": {
+                            "fontWeight": "400"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.4444444444444446,
+            "hiding": "normal",
+            "lengthMode": "fill",
+            "location": "bottom",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0,
+            "opacity": "adaptive"
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
